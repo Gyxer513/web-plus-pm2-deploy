2a8200a995916d72c5abb4c1d67f11721a41dc5e
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2023-06-21 23:46:28 +0300

    helmet added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,14 +8,14 @@ import { errors } from 'celebrate';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
-
+import helmet from 'helmet';
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
 // app.use(cors())
-
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
