ab69f801f5f12d8e0fb4b8ec58e364d7056ca51f
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2023-06-22 00:18:38 +0300

    helmet added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,22 +11,18 @@ import routes from './routes';
 import helmet from 'helmet';
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(helmet());
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-app.use(cors({
-  origin: [
-    'http://www.gyxer.com',
-    'https://www.gyxer.com',
-  ],
-}))
-app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
+
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log('server is running'));
