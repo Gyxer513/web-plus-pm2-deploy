b164c01e574e3dc932e6d74539b945ad9e55740d
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2023-06-22 01:09:06 +0300

    helmet added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,6 +8,7 @@ import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
+import helmet from "helmet";
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
@@ -18,8 +19,7 @@ app.get('/crash-test', () => {
       throw new Error('Сервер сейчас упадёт');
     }, 0);
   });
-  
-app.use(cors());
+app.use(helmet());  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
