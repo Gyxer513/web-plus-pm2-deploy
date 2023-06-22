fa1496c4936bf42087dfddda242424c3ade248b3
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2023-06-22 07:02:43 +0300

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,31 +1,34 @@
 import 'dotenv/config';
 
-import express from 'express';
+import express, { Request } from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
-import helmet from "helmet";
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-app.get('/crash-test', () => {
-    setTimeout(() => {
-      throw new Error('Сервер сейчас упадёт');
-    }, 0);
-  });
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://localhost:3000',
-  ],
-}))
-app.use(helmet());  
+const allowlist = ['http://localhost:3000', 'https://localhost:3000'];
+const corsOptionsDelegate = (
+  req: Request,
+  callback: (error: Error | null, corsOptions?: CorsOptions) => void,
+) => {
+  const corsOptions = { origin: false };
+  const header = req.header('Origin');
+
+  if (header !== undefined && allowlist.indexOf(header) !== -1) {
+    corsOptions.origin = true; // reflect (enable) the requested origin in the CORS response
+  }
+
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
+app.use(cors(corsOptionsDelegate));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
